1186e4e676609d2de8239497305b13f81e8992a1
MashaWarface <161467481+MashaWarface@users.noreply.github.com>
2024-03-26 22:05:28 +0700

    Create main.c

---
+++ lab8-1/main.c
@@ -0,0 +1,17 @@
+#include "prim.h"
+#include "stdio.h"
+
+int main(void) {
+    int NodeCount, EdgesCount;
+    unsigned int **Matrix = NULL;
+
+    scanf("%d %d", &NodeCount, &EdgesCount);
+
+    if (NodeCount == 1) {
+        puts("");
+        return 0;
+    }
+
+    primMST(Matrix, NodeCount, EdgesCount);
+    return 0;
+}
